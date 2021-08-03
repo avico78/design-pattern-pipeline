0f0d82cbc932f08f5c389fa4c132722859426725
Avi <avico78@gmail.com>
2021-08-03 14:00:40 +0300

    review

--- client.py
+++ client.py
@@ -1,25 +1,55 @@
-from context_task import TaskType,TaskContext
+from context_task import TaskType,TaskContext,Workflow
 #from interface_data import DataLoadFactory
 #from interface_transformation import DataTransformationFactory
 
 configs = [ {'operation': TaskType.LOAD,
      "task" : "some_dict",
      "source": "csv",
+     "task_seq": 1,
      "task_params": {"path": "aaa/to/csv1"}
      },
      {'operation': TaskType.LOAD,
      "task" : "some_dict",
+     "task_seq": 1,
      "source": "csv",
      "task_params": {"path": "bbb/to/csv2"}
      }]
 
 
 
+workflow = Workflow()
+
+
+
+
 tasks = [TaskContext.get_task(config) for config in configs]
 
-a = [task.execute_task() for task in tasks]
+workflow.add_task(tasks[0])
+workflow.add_task(tasks[1])
 
-print(a)
+print(workflow.show_tasks())
+workflow.append_task(1,{"1":3 })
+workflow.append_task(9,{"1":4 })
+workflow.append_task(2,{"1":8 })
+workflow.append_task(5,{"1":10 })
+workflow.append_task(8,{"1":100 })
+a = workflow.get_workflow_bulk_plan()
+print(a.keys())
+
+
+# od = dict(sorted(a.items(), key=lambda item: item[1]))
+# print(od)
+# a = [task.execute_task() for task in tasks]
+
+# print(a)
+
+
+
+
+
+
+
+
 # # print(configs.get('params'))
 # # exit()
 # # dataload_context = TaskContext()
--- context_task.py
+++ context_task.py
@@ -54,38 +54,77 @@ class DataLoadFactory(TaskFactory):
     def __init__(self, config: Dict):
         self.task = config.get('task')
         self.task_params = config.get('task_params')
+        self.task_seq = config.get('task_seq')
 
     def get_task(self):
         return self.task
-
+    
     def execute_task(self) -> Callable :
         return getattr(DayaframeTransformation, self.task)(self.task_params)
 
-
-        #return f"Task {config.get('task')}"
-        
-    # def __init__(self, execution_step: Callable, depends_on: "Task" = None,
-    #              **kwargs) -> None:
-    #     self.depends_on = depends_on
-    #     self.execution_step = execution_step
-    #     self._kwargs = kwargs
-
-      
-    # def construct_task(config: Dict) -> "Task": 
-    #     @property
-    #     def task(self)   :
-    #         return "spme"
-    #     # print("Here", config.get('task'))
-    #     # print("selee", task_state)
-        
-
- 
 
 
 class DataTransformationFactory(TaskFactory):
     "The DataLoad Factory Class"
 
+def append_value(dict_obj, key, value):
+    # Check if key exist in dict or not
+    if key in dict_obj:
+        # Key exist in dict.
+        # Check if type of value of key is list or not
+        if not isinstance(dict_obj[key], list):
+            # If type is not list then make it list
+            dict_obj[key] = [dict_obj[key]]
+        # Append the value in list
+        dict_obj[key].append(value)
+    else:
+        # As key is not in dict,
+        # so, add key-value pair
+        dict_obj[key] = value
+# Dictionary of strings and ints
+word_freq = {1: {"some": 12},2: {"some": 12},3: {"some": 12}
+}
 
+append_value(word_freq, 1, {"some": 1222})
+print(word_freq)
+
+class TaskBulk:
+    task_start = 0
+
+    def __init__(self):
+        self.task_seq = []
+ 
+
+
+class Workflow:
+    workflow_task_bulk = {}
+
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, task: DataLoadFactory):
+        self.tasks.append(task)
+
+    def show_tasks(self):
+        return [task.get_task() for task in self.tasks]
+
+    def append_task(self,key, value):
+        if key in self.workflow_task_bulk:
+            # Key exist in dict.
+            # Check if type of value of key is list or not
+            if not isinstance(self.workflow_task_bulk[key], list):
+                # If type is not list then make it list
+                self.workflow_task_bulk[key] = [self.workflow_task_bulk[key]]
+            # Append the value in list
+            self.workflow_task_bulk[key].append(value)
+        else:
+            # As key is not in dict,
+            # so, add key-value pair
+            self.workflow_task_bulk[key] = value
+        print(self.workflow_task_bulk)
+    
+    def get_workflow_bulk_plan(self):
+        return self.workflow_task_bulk
 
 class TaskContext:
     available_factories = {
